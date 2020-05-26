e11181d333e660be896954966d6d86be43b5edac
zhaopengfei <1027926875@qq.com>
2020-05-26 22:11:14 +0800

    thread base

---
+++ src/main/java/com/eru/concurrency/createthreads/wrongways/Lambda.java
@@ -0,0 +1,19 @@
+package com.eru.concurrency.createthreads.wrongways;
+
+import javafx.util.Builder;
+
+/**
+ * Lambda创建线程
+ * Created by eru on 2020/5/26.
+ */
+public class Lambda {
+    public static void main(String[] args) {
+        Builder<Thread> t = () -> new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        t.build().start();
+    }
+}
